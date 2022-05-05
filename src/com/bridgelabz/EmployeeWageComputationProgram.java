5c098d20ab199e53c6401e4b16e53739e3f1aad9
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-05-05 22:34:02 +0530

    Added: UC10

--- src/com/bridgelabz/EmployeeWageComputationProgram.java
+++ src/com/bridgelabz/EmployeeWageComputationProgram.java
@@ -1,55 +1,52 @@
 package com.bridgelabz;
 
 class EmployeeWageBuilder {
-    public static final int FULL_DAY_HOUR = 8;
-    public static final int PART_TIME_HOUR = 4;
-    public static final int IS_FULL_DAY_PRESENT = 1;
-    public static final int IS_HALF_DAY_PRESENT = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int FULL_DAY_HR = 8;
+    public static final int PART_DAY_HR = 4;
 
-    private String companies;
-    private int wagePerHour = 20;
-    private int workingDays = 22;
-    private int workingHoursPerMonth = 80;
+    private CompanyEmpWage[] companyEmpWagerArray= new CompanyEmpWage[5];
+    private int numOfCompany = 0;
 
-    int companyEmpWage() {
-        int dailyWage = 0;
-        int totalEmpWage = 0;
-        int totalHours = 0;
-
-        int totalWorkingDays = 0;
-        while (totalWorkingDays < workingHoursPerMonth && totalHours <= workingDays) {
-            int empCheck = (int) (Math.random() * 10) % 3;
-            //System.out.println("Total Working Hours of an Employee for a Month: " + totalHours);
-            switch (empCheck) {
-                case IS_FULL_DAY_PRESENT:
-                    totalHours += FULL_DAY_HOUR;
+    private void addCompanyEmpWage(String companyName, int empWagePerHr,  int numMaxWorkingDay, int maxHrsInMonth) {
+        CompanyEmpWage empWage = new CompanyEmpWage(companyName , empWagePerHr , numMaxWorkingDay , maxHrsInMonth);
+        companyEmpWagerArray[numOfCompany] = empWage;
+        numOfCompany++;
+    }
+    private void computeEmpWageFromArray() {
+        for (int i= 0; i< numOfCompany; i++){
+            int totalWage =computeEmpWage(companyEmpWagerArray[i]);
+            companyEmpWagerArray[i].totalEmpWage= totalWage;
+            System.out.println(companyEmpWagerArray[i]);
+        }
+    }
+    private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+        int totalEmpHours = 0, totalWorkingDays = 0, workingHours = 0;
+        while (totalEmpHours < companyEmpWage.maxHrsInMonth && totalWorkingDays < companyEmpWage.numMaxWorkingDay) {
+            totalWorkingDays++;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            // Calculating Wage On Employee Type
+            switch ((int) empCheck) {
+                case 0:
+                    workingHours = FULL_DAY_HR;
                     break;
-                case IS_HALF_DAY_PRESENT:
-                    totalHours += PART_TIME_HOUR;
+                case 1:
+                    workingHours = PART_DAY_HR;
                     break;
                 default:
-                    dailyWage = 0;
+                    workingHours = 0;
             }
+            totalEmpHours += workingHours;
             totalWorkingDays++;
-            dailyWage = wagePerHour * totalHours;
-            totalEmpWage += dailyWage;
-            //System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
         }
-        //System.out.println("Total Employee Wage for a Month: " + totalEmpWage);
-        return totalEmpWage;
+        return totalEmpHours * companyEmpWage.empWagePerHr;
     }
-}
 
-public class EmployeeWageComputationProgram {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        System.out.println();
-        EmployeeWageBuilder obj1 = new EmployeeWageBuilder();
-        EmployeeWageBuilder obj2 = new EmployeeWageBuilder();
-        EmployeeWageBuilder obj3 = new EmployeeWageBuilder();
-
-        System.out.println("Total Emp Wage of Company XYZ: " + obj1.companyEmpWage());
-        System.out.println("Total Emp Wage of Company GFG: " + obj2.companyEmpWage());
-        System.out.println("Total Emp Wage of Company MEG: " + obj3.companyEmpWage());
+        EmployeeWageBuilder empWageBuilder = new EmployeeWageBuilder();
+        empWageBuilder.addCompanyEmpWage("XYZ",22,18,80);
+        empWageBuilder.addCompanyEmpWage("MFG",20,20,90);
+        empWageBuilder.computeEmpWageFromArray();
     }
 }
