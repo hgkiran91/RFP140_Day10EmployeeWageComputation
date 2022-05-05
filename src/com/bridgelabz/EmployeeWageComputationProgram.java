06bed2a5431e11e4ba2d5515c1e7f40d04e28878
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-05-05 16:25:22 +0530

    Added: UC8

--- src/com/bridgelabz/EmployeeWageComputationProgram.java
+++ src/com/bridgelabz/EmployeeWageComputationProgram.java
@@ -1,46 +1,62 @@
 package com.bridgelabz;
 
 class EmployeeWage {
-    public static final int WAGE_PER_HOUR = 20;
     public static final int FULL_DAY_HOUR = 8;
     public static final int PART_TIME_HOUR = 4;
     public static final int IS_FULL_DAY_PRESENT = 1;
     public static final int IS_HALF_DAY_PRESENT = 2;
-    public static final int MAX_WORKING_DAYS_PER_MONTH = 20;
-    public static final int TOTAL_WORKING_HOURS_PER_MONTH = 100;
 
-    public static void totalWage() {
+    private String companies;
+    private int wagePerHour;
+    private int workingDays;
+    private int workingHoursPerMonth;
+
+    public EmployeeWage(String companies, int wagePerHour, int workingDays, int workingHoursPerMonth) {
+        this.companies=companies;
+        this.wagePerHour=wagePerHour;
+        this.workingDays=workingDays;
+        this.workingHoursPerMonth=workingHoursPerMonth;
+    }
+
+    double companyEmpWage() {
         int dailyWage = 0;
         int totalEmpWage = 0;
         int totalHours = 0;
 
         int totalWorkingDays = 0;
-        while (totalWorkingDays < MAX_WORKING_DAYS_PER_MONTH && totalHours <= TOTAL_WORKING_HOURS_PER_MONTH) {
+        while (totalWorkingDays < workingHoursPerMonth && totalHours <= workingDays) {
             int empCheck = (int) (Math.random() * 10) % 3;
-            System.out.println("Total Working Hours of an Employee for a Month: " + totalHours);
+            //System.out.println("Total Working Hours of an Employee for a Month: " + totalHours);
             switch (empCheck) {
                 case IS_FULL_DAY_PRESENT:
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
                     totalHours += FULL_DAY_HOUR;
                     break;
                 case IS_HALF_DAY_PRESENT:
-                    dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
                     totalHours += PART_TIME_HOUR;
                     break;
                 default:
                     dailyWage = 0;
             }
             totalWorkingDays++;
+            dailyWage = wagePerHour * totalHours;
             totalEmpWage += dailyWage;
-            System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
+            //System.out.println(empCheck + " Daily Wage of an Employee: " + dailyWage);
         }
-        System.out.println("Total Employee Wage for a Month: " + totalEmpWage);
+        //System.out.println("Total Employee Wage for a Month: " + totalEmpWage);
+        return totalEmpWage;
     }
 }
 
 public class EmployeeWageComputationProgram {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        EmployeeWage.totalWage();
+        System.out.println();
+        EmployeeWage obj1 = new EmployeeWage("XYZ", 20, 24, 80);
+        EmployeeWage obj2 = new EmployeeWage("GFG", 25, 22, 90);
+        EmployeeWage obj3 = new EmployeeWage("MEG", 24, 23, 100);
+
+        System.out.println("Total Emp Wage of Company XYZ: "+obj1.companyEmpWage());
+        System.out.println("Total Emp Wage of Company GFG: "+obj2.companyEmpWage());
+        System.out.println("Total Emp Wage of Company MEG: "+obj3.companyEmpWage());
     }
 }
